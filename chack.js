5c10c910fcc878fe0bcaefc5c3d4d5c589c72f6e
asd10384 <65000786+asd10384@users.noreply.github.com>
2021-02-14 17:26:36 +0900

    임베드로 확인

--- chack.js
+++ chack.js
@@ -2,10 +2,7 @@
 const request = require("request");
 const cheerio = require("cheerio");
 const http = require('http');
-const { readdirSync } = require('fs');
-const { join } = require('path');
-const { hostname } = require("os");
-
+const { writeFileSync } = require('fs');
 
 var url = `http://ytms.netlify.app`;
 request(url, async function (err, res, html) {
@@ -18,43 +15,41 @@ request(url, async function (err, res, html) {
             var v = $(this).children('a.vocal').text().trim();
             var l = $(this).children('a.link').text().trim();
             var yturl = l.replace(chack, '').replace(/(?:&(.+))/gi, '');
-            text += `<div id="text">${v}-${n}</div>
-            <div id="iframe">
-                <iframe width="640" height="480" src="http://www.youtube.com/embed/${yturl}" frameborder="0" allowfullscreen></iframe>
-            </div>`;
+            text += `<div id="text">${v}-${n}</div><div id="iframe"><iframe width="640" height="480" src="http://www.youtube.com/embed/${yturl}" frameborder="0" allowfullscreen></iframe></div>\n`;
         });
-        var app = http.createServer(async function (req, res) {
-            res.writeHead(200);
-            res.end(`<!DOCTYPE html><html lang="ko">
-            <head>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>음악들임베드</title>
-            </head>
-        
-            <body>
-                <style>
-                    body {
-                        padding:10px;
-                        background-color:rgb(80, 80, 80);
-                        align-items: center;
-                        color: white;
-                    }
+        writeFileSync('./html.txt', text, async function (err) {
+            if (!err) {
+                var app = http.createServer(async function (req, res) {
+                    res.writeHead(200);
+                    res.end(`<!DOCTYPE html><html lang="ko">
+                        <head>
+                            <meta charset="UTF-8">
+                            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                            <title>음악들임베드</title>
+                        </head>
                     
-                    iframe[src*="http://www.youtube.com/embed/"] {
-                        display:block;
-                        margin:0px auto 10px;
-                        border:2px solid white;
-                    }
-                </style>
-                <div>
-                    ${text}
-                </div>
-            </body>
-        </html>`);
+                        <body>
+                            <style>
+                                body {
+                                    padding:10px;
+                                    background-color:rgb(80, 80, 80);
+                                    align-items: center;
+                                    color: white;
+                                }
+                                
+                                iframe[src*="http://www.youtube.com/embed/"] {
+                                    display:block;
+                                    margin:0px auto 10px;
+                                    border:2px solid white;
+                                }
+                            </style>
+                            <div>${text}</div>
+                        </body></html>`);
+                });
+                app.listen(25565);
+                console.log(`http://localhost:25565`);
+            }
+
         });
-        app.listen(25565);
-        console.log(`http://localhost:25565`);
-        
     }
 });
