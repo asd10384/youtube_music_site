3c7c0929ce1b5bf153aec3993ec31ff8a4d3b4b8
asd10384 <65000786+asd10384@users.noreply.github.com>
2021-02-14 18:00:21 +0900

    Update chack.js

--- chack.js
+++ chack.js
@@ -20,7 +20,7 @@ request(url, async function (err, res, html) {
             var l = $(this).children('a.link').text().trim();
             var yturl = l.replace(chack, '').replace(/(?:&(.+))/gi, '');
             text += `<div id="m"><div id="text">${v}-${n}</div><div id="iframe"><iframe id="ytplayer" type="text/html" width="480" height="320" src="http://www.youtube.com/embed/${yturl}" frameborder="0" allowfullscreen></iframe></div>\n`;
-            text += `<script>var tag = document.createElement('script');tag.src = "https://www.youtube.com/player_api";var firstScriptTag = document.getElementsByTagName('script')[0];firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);var player;function onYouTubePlayerAPIReady() {player = new YT.Player('ytplayer', {height: '360',width: '640',videoId: 'M7lc1UVf-VE'});}</script></div>`;
+            text += `<script>var tag = document.createElement('script');tag.src = "https://www.youtube.com/player_api";var firstScriptTag = document.getElementsByTagName('script')[0];firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);var player;function onYouTubePlayerAPIReady() {player = new YT.Player('ytplayer', {height: '360',width: '640',videoId: '${yturl}'});}</script></div>`;
         });
         writeFileSync('./html.txt', text, async function (err) {
             if (!err) {
