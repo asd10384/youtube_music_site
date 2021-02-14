184dfc4d9146844b5f139fc783d1d13bf9d5a73f
asd10384 <65000786+asd10384@users.noreply.github.com>
2021-02-14 17:58:50 +0900

    오류수정

--- chack.js
+++ chack.js
@@ -1,4 +1,9 @@
 
+/*
+    npm i
+    node chack.js
+*/
+
 const request = require("request");
 const cheerio = require("cheerio");
 const { writeFileSync } = require('fs');
@@ -14,7 +19,8 @@ request(url, async function (err, res, html) {
             var v = $(this).children('a.vocal').text().trim();
             var l = $(this).children('a.link').text().trim();
             var yturl = l.replace(chack, '').replace(/(?:&(.+))/gi, '');
-            text += `<div id="text">${v}-${n}</div><div id="iframe"><iframe width="640" height="480" src="http://www.youtube.com/embed/${yturl}" frameborder="0" allowfullscreen></iframe></div>\n`;
+            text += `<div id="m"><div id="text">${v}-${n}</div><div id="iframe"><iframe id="ytplayer" type="text/html" width="480" height="320" src="http://www.youtube.com/embed/${yturl}" frameborder="0" allowfullscreen></iframe></div>\n`;
+            text += `<script>var tag = document.createElement('script');tag.src = "https://www.youtube.com/player_api";var firstScriptTag = document.getElementsByTagName('script')[0];firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);var player;function onYouTubePlayerAPIReady() {player = new YT.Player('ytplayer', {height: '360',width: '640',videoId: 'M7lc1UVf-VE'});}</script></div>`;
         });
         writeFileSync('./html.txt', text, async function (err) {
             if (!err) {
