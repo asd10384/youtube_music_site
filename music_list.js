66e3972c2bf78d627dcda41a23fdea7d9f231031
asd10384 <65000786+asd10384@users.noreply.github.com>
2021-03-14 18:38:39 +0900

    대대적인 공사 뼈대

---
+++ music_list.js
@@ -0,0 +1,48 @@
+[
+    {
+        "노래퀴즈": {
+            "1": [
+                {
+                    "name": "K-POP",
+                    "complite": true
+                },
+                {
+                    "name": "J-POP",
+                    "complite": false
+                }
+            ],
+            "2": [
+                {
+                    "name": "test입니다.",
+                    "complite": false
+                },
+                {
+                    "name": "test2입니다.",
+                    "complite": false
+                }
+            ]
+        },
+        "테스트퀴즈": {
+            "1": [
+                {
+                    "name": "test1-1",
+                    "complite": false
+                },
+                {
+                    "name": "test1-2",
+                    "complite": false
+                }
+            ],
+            "2": [
+                {
+                    "name": "test2-1",
+                    "complite": false
+                },
+                {
+                    "name": "test2-2",
+                    "complite": false
+                }
+            ]
+        }
+    }
+]
