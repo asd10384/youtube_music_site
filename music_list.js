cf3592bcf651ccd9c925ecbec48c4d7864062dd0
Kacarong <hky4258@naver.com>
2021-03-27 00:00:08 +0900

    1

--- music_list.js
+++ music_list.js
@@ -22,24 +22,24 @@
                 }
             }
         },
-        "테스트퀴즈": {
-            "테스트1": {
-                "test1-1": {
-                    "url": "https://ytms.netlify.app/testquiz/test1/test1-1.html",
+        "POP-MIX": {
+            "MIX-A": {
+                "Q-1": {
+                    "url": "https://ytms.netlify.app/POP-MIX/MIX-A/Q-1.html",
                     "complite": false
                 },
-                "test1-2": {
-                    "url": "https://ytms.netlify.app/testquiz/test1/test1-2.html",
+                "Q-2": {
+                    "url": "https://ytms.netlify.app/POP-MIX/MIX-A/Q-2.html",
                     "complite": false
                 }
             },
-            "테스트2": {
-                "test2-1": {
-                    "url": "https://ytms.netlify.app/testquiz/test2/test2-1.html",
+            "MIX-B": {
+                "Q-3": {
+                    "url": "https://ytms.netlify.app/POP-MIX/MIX-B/Q-3.html",
                     "complite": false
                 },
-                "test2-2": {
-                    "url": "https://ytms.netlify.app/testquiz/test2/test2-2.html",
+                "Q-4": {
+                    "url": "https://ytms.netlify.app/POP-MIX/MIX-B/Q-4.html",
                     "complite": false
                 }
             }
