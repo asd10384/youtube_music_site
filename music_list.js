8b30292ccf7401d2f58a2e86df4f8377a0019a0e
Kacarong <hky4258@naver.com>
2021-03-28 21:44:46 +0900

    노래 퀴즈 수정

--- music_list.js
+++ music_list.js
@@ -9,15 +9,13 @@
                 "Q-2": {
                     "url": "https://ytms.netlify.app/K-POP-MIX/MIX-A/Q-2.html",
                     "complite": true
-                }
-            },
-            "MIX-B": {
+                },
                 "Q-3": {
-                    "url": "https://ytms.netlify.app/K-POP-MIX/MIX-B/Q-3.html",
+                    "url": "https://ytms.netlify.app/K-POP-MIX/MIX-A/Q-3.html",
                     "complite": true
                 },
                 "Q-4": {
-                    "url": "https://ytms.netlify.app/K-POP-MIX/MIX-B/Q-4.html",
+                    "url": "https://ytms.netlify.app/K-POP-MIX/MIX-A/Q-4.html",
                     "complite": true
                 }
             }
@@ -31,15 +29,13 @@
                 "Q-2": {
                     "url": "https://ytms.netlify.app/POP-MIX/MIX-A/Q-2.html",
                     "complite": false
-                }
-            },
-            "MIX-B": {
+                },
                 "Q-3": {
-                    "url": "https://ytms.netlify.app/POP-MIX/MIX-B/Q-3.html",
+                    "url": "https://ytms.netlify.app/POP-MIX/MIX-A/Q-3.html",
                     "complite": false
                 },
                 "Q-4": {
-                    "url": "https://ytms.netlify.app/POP-MIX/MIX-B/Q-4.html",
+                    "url": "https://ytms.netlify.app/POP-MIX/MIX-A/Q-4.html",
                     "complite": false
                 }
             }
