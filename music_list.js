db14d7d104aba924b61190169389d3d6115e6c28
asd10384 <tmdgks0466@naver.com>
2021-05-02 01:07:22 +0900

    추가

--- music_list.js
+++ music_list.js
@@ -4,22 +4,32 @@
             "MIX-A": {
                 "Q-1": {
                     "url": "https://ytms.netlify.app/K-POP-MIX/MIX-A/Q-1.html",
+                    "desc": "1번 한국노래 입니다.\n띄어쓰기는 이렇게 표시합니다.",
+                    "musicquiz": true,
                     "complite": true
                 },
                 "Q-2": {
                     "url": "https://ytms.netlify.app/K-POP-MIX/MIX-A/Q-2.html",
+                    "desc": "2번 한국노래 입니다.",
+                    "musicquiz": true,
                     "complite": true
                 },
                 "Q-3": {
                     "url": "https://ytms.netlify.app/K-POP-MIX/MIX-A/Q-3.html",
+                    "desc": "3번 한국노래 입니다.",
+                    "musicquiz": true,
                     "complite": true
                 },
                 "Q-4": {
                     "url": "https://ytms.netlify.app/K-POP-MIX/MIX-A/Q-4.html",
+                    "desc": "4번 한국노래 입니다.",
+                    "musicquiz": true,
                     "complite": true
                 },
                 "Q-5": {
                     "url": "https://ytms.netlify.app/K-POP-MIX/MIX-A/Q-5.html",
+                    "desc": "5번 한국노래 입니다.",
+                    "musicquiz": true,
                     "complite": true
                 }
             }
@@ -28,18 +38,26 @@
             "MIX-A": {
                 "Q-1": {
                     "url": "https://ytms.netlify.app/POP-MIX/MIX-A/Q-1.html",
+                    "desc": "",
+                    "musicquiz": true,
                     "complite": false
                 },
                 "Q-2": {
                     "url": "https://ytms.netlify.app/POP-MIX/MIX-A/Q-2.html",
+                    "desc": "",
+                    "musicquiz": true,
                     "complite": false
                 },
                 "Q-3": {
                     "url": "https://ytms.netlify.app/POP-MIX/MIX-A/Q-3.html",
+                    "desc": "",
+                    "musicquiz": true,
                     "complite": false
                 },
                 "Q-4": {
                     "url": "https://ytms.netlify.app/POP-MIX/MIX-A/Q-4.html",
+                    "desc": "",
+                    "musicquiz": true,
                     "complite": false
                 }
             }
