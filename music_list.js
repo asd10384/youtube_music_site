64f605648f94d94be76c8c06764ffae5b43a5282
asd10384 <tmdgks0466@naver.com>
2021-05-03 23:29:26 +0900

    형식 변경

--- music_list.js
+++ music_list.js
@@ -1,50 +1,55 @@
+
+/*
+    # 세팅 설명 #
+
+    url 에 사용할수 있는 형식
+    [ https://ytms.netlify.app/(첫번째 폴더 이름)/(첫번째 폴더안 폴더이름)/(파일이름) ]
+
+    desc 에 사용할수 있는 형식
+    [ 텍스트 (줄바꿈은 \n 으로 가능) ]
+
+    quiz 에 사용할수 있는 형식
+    [ 음악퀴즈 , 그림퀴즈 ]
+
+    complite 에 사용할수 있는 형식
+    [ true, false ]
+
+    # 파일 쓰는방식 #
+    example폴더 참고바람
+*/
+
 [
     {
         "K-POP-MIX": {
             "MIX-A": {
                 "Q-1": {
                     "url": "https://ytms.netlify.app/K-POP-MIX/MIX-A/Q-1.html",
-                    "desc": "1번 한국노래 입니다.\n띄어쓰기는 이렇게 표시합니다.",
-                    "quiz": {
-                        "music": true,
-                        "format": ""
-                    },
+                    "desc": "1번 한국노래 입니다.\n이런식으로 줄을 바꿔 쓸수 있습니다.",
+                    "quiz": "음악퀴즈",
                     "complite": true
                 },
                 "Q-2": {
                     "url": "https://ytms.netlify.app/K-POP-MIX/MIX-A/Q-2.html",
                     "desc": "2번 한국노래 입니다.",
-                    "quiz": {
-                        "music": true,
-                        "format": ""
-                    },
+                    "quiz": "음악퀴즈",
                     "complite": true
                 },
                 "Q-3": {
                     "url": "https://ytms.netlify.app/K-POP-MIX/MIX-A/Q-3.html",
                     "desc": "3번 한국노래 입니다.",
-                    "quiz": {
-                        "music": true,
-                        "format": ""
-                    },
+                    "quiz": "음악퀴즈",
                     "complite": true
                 },
                 "Q-4": {
                     "url": "https://ytms.netlify.app/K-POP-MIX/MIX-A/Q-4.html",
                     "desc": "4번 한국노래 입니다.",
-                    "quiz": {
-                        "music": true,
-                        "format": ""
-                    },
+                    "quiz": "음악퀴즈",
                     "complite": true
                 },
                 "Q-5": {
                     "url": "https://ytms.netlify.app/K-POP-MIX/MIX-A/Q-5.html",
                     "desc": "5번 한국노래 입니다.",
-                    "quiz": {
-                        "music": true,
-                        "format": ""
-                    },
+                    "quiz": "음악퀴즈",
                     "complite": true
                 }
             }
@@ -53,38 +58,26 @@
             "MIX-A": {
                 "Q-1": {
                     "url": "https://ytms.netlify.app/POP-MIX/MIX-A/Q-1.html",
-                    "desc": "",
-                    "quiz": {
-                        "music": true,
-                        "format": ""
-                    },
+                    "desc": "그림퀴즈 테스트 입니다.",
+                    "quiz": "그림퀴즈",
                     "complite": false
                 },
                 "Q-2": {
                     "url": "https://ytms.netlify.app/POP-MIX/MIX-A/Q-2.html",
                     "desc": "",
-                    "quiz": {
-                        "music": true,
-                        "format": ""
-                    },
+                    "quiz": "",
                     "complite": false
                 },
                 "Q-3": {
                     "url": "https://ytms.netlify.app/POP-MIX/MIX-A/Q-3.html",
                     "desc": "",
-                    "quiz": {
-                        "music": true,
-                        "format": ""
-                    },
+                    "quiz": "",
                     "complite": false
                 },
                 "Q-4": {
                     "url": "https://ytms.netlify.app/POP-MIX/MIX-A/Q-4.html",
                     "desc": "",
-                    "quiz": {
-                        "music": true,
-                        "format": ""
-                    },
+                    "quiz": "",
                     "complite": false
                 }
             }
