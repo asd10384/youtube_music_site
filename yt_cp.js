61d4f595da4490c77bbd041ef0e3f86f6a2a6c68
asd10384 <65000786+asd10384@users.noreply.github.com>
2021-07-12 22:34:24 +0900

    전체 파일 비교 제작

--- yt_cp.js
+++ yt_cp.js
@@ -12,36 +12,76 @@
  * npm install 을 터미널에 입력합니다.
  * 
  * 사용법
- * PLAYLIST 에 플레이리스트 주소 , FILE 에는 생성될 파일 이름을 적습니다.
- * 
  * node yt_cp.js 를 입력해 시작합니다.
  * 
  * 예시로 automake.html 파일을 제작했습니다.
 */
 
-const FILE1 = 'K-POP-MIX/MIX-A/Q-8';
-const FILE2 = 'K-POP-MIX/MIX-A/Q-12';
+const FILE1 = 'K-POP-MIX/MIX-A/Q-1';
+const FILE_SET = '파일비교결과';
 
 
 
 const fs = require('fs');
 
-var file1_list = [],
-    file2_list = [];
+var file1_list = fs.readdirSync('./'),
+    file2_list = [],
+    file3_list = [],
+    remove = [],
+    allfile = [];
+for (file1 of file1_list) {
+    if (file1 == '.git' || file1 == 'image' || file1 == 'node_modules') continue;
+    if (fs.lstatSync(`./${file1}`).isDirectory()) {
+        file2_list = fs.readdirSync(`./${file1}/`);
+        if (file2_list) {
+            for (file2 of file2_list) {
+                if (fs.lstatSync(`./${file1}/${file2}`).isDirectory()) {
+                    file3_list = fs.readdirSync(`./${file1}/${file2}/`);
+                    remove = [];
+                    for (i in file3_list) {
+                        file3_list[i] = `${file1}/${file2}/${file3_list[i]}`;
+                        if (file3_list[i] == `${FILE1}.html`) {
+                            remove.push(i);
+                        }
+                    }
+                    for (r of remove) {
+                        file3_list.splice(r, 1);
+                    }
+                    allfile = allfile.concat(file3_list);
+                }
+            }
+        }
+    }
+}
+
+file1_list = [];
+file2_list = [];
+var text = '';
 fs.readFileSync(`${FILE1}.html`, {encoding:'utf-8'}).replace(/\<a class\=\"name\"\>.*\<\/a\> \-/gi, (txt) => {
     file1_list.push(txt.replace(/\<a class\=\"name\"\>|\<\/a\> \-/gi,''));
 });
-fs.readFileSync(`${FILE2}.html`, {encoding:'utf-8'}).replace(/\<a class\=\"name\"\>.*\<\/a\> \-/gi, (txt) => {
-    file2_list.push(txt.replace(/\<a class\=\"name\"\>|\<\/a\> \-/gi,''));
-});
-
-var text;
-console.log(`\n비교 시작!\n`);
-for (i in file1_list) {
-    text = file1_list[i];
-    if (file2_list.includes(text)) {
-        console.log(`"${text}" 중복! 1번파일: ${Number(i)+1}번 , 2번파일: ${Number(file2_list.indexOf(text))+1}`);
+fs.writeFileSync(`./${FILE_SET}.txt`, '', {encoding:'utf-8'});
+makefile(`\n잠시뒤 "${FILE1}"과 총 ${allfile.length}개의 파일을 비교합니다.\n`);
+setTimeout(() => {
+    for (FILE2 of allfile) {
+        file2_list = [];
+        makefile(`${FILE2}파일과 비교 하는중`);
+        fs.readFileSync(`${FILE2}`, {encoding:'utf-8'}).replace(/\<a class\=\"name\"\>.*\<\/a\> \-/gi, (txt) => {
+            file2_list.push(txt.replace(/\<a class\=\"name\"\>|\<\/a\> \-/gi,''));
+            for (i in file1_list) {
+                text = file1_list[i];
+                if (file2_list.includes(text)) {
+                    makefile(`중복발견 => 제목: ${text} , 1번파일: ${Number(i)+1}번 , 2번파일: ${Number(file2_list.indexOf(text))+1}`);
+                }
+            }
+        });
+        makefile(`${FILE2}파일과 비교 끝`);
     }
-}
-console.log(`\n비교 끝!\n`);
+    makefile(`\n비교 끝\n`);
+}, 3500);
 
+
+function makefile(text = '') {
+    console.log(text);
+    fs.appendFileSync(`./${FILE_SET}.txt`, `${text}\n`, {encoding:'utf-8'});
+}
