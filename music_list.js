3e6a32643ab35f32b5209199169cce3653c3278c
Kacarong <hky4258@naver.com>
2021-07-13 21:29:48 +0900

    MIX-A를 MIX-B로 분할하는 작업 (K)

--- music_list.js
+++ music_list.js
@@ -27,7 +27,7 @@
             "MIX-A": {
                 "Q-1": {
                     "url": "https://ytms.netlify.app/K-POP-MIX/MIX-A/Q-1.html",
-                    "desc": "1번 한국노래 입니다.\n이런식으로 줄을 바꿔 쓸수 있습니다.",
+                    "desc": "1번 한국노래 입니다.",
                     "quiz": "음악퀴즈",
                     "complite": 100,
                     "start": true
@@ -94,73 +94,75 @@
                     "quiz": "음악퀴즈",
                     "complite": 100,
                     "start": true
-                },
-                "Q-11": {
-                    "url": "https://ytms.netlify.app/K-POP-MIX/MIX-A/Q-11.html",
-                    "desc": "11번 한국노래 입니다.",
+                }
+            },
+            "MIX-B": {
+                "Q-1": {
+                    "url": "https://ytms.netlify.app/K-POP-MIX/MIX-B/Q-1.html",
+                    "desc": "1번 한국노래 입니다.",
                     "quiz": "음악퀴즈",
                     "complite": 100,
                     "start": true
                 },
-                "Q-12": {
-                    "url": "https://ytms.netlify.app/K-POP-MIX/MIX-A/Q-12.html",
-                    "desc": "12번 한국노래 입니다.",
+                "Q-2": {
+                    "url": "https://ytms.netlify.app/K-POP-MIX/MIX-B/Q-2.html",
+                    "desc": "2번 한국노래 입니다.",
                     "quiz": "음악퀴즈",
                     "complite": 100,
                     "start": true
                 },
-                "Q-13": {
-                    "url": "https://ytms.netlify.app/K-POP-MIX/MIX-A/Q-13.html",
-                    "desc": "13번 한국노래 입니다.",
+                "Q-3": {
+                    "url": "https://ytms.netlify.app/K-POP-MIX/MIX-B/Q-3.html",
+                    "desc": "3번 한국노래 입니다.",
                     "quiz": "음악퀴즈",
                     "complite": 0,
                     "start": false
                 },
-                "Q-14": {
-                    "url": "https://ytms.netlify.app/K-POP-MIX/MIX-A/Q-14.html",
-                    "desc": "14번 한국노래 입니다.",
+                "Q-4": {
+                    "url": "https://ytms.netlify.app/K-POP-MIX/MIX-B/Q-4.html",
+                    "desc": "4번 한국노래 입니다.",
                     "quiz": "음악퀴즈",
                     "complite": 0,
                     "start": false
                 },
-                "Q-15": {
-                    "url": "https://ytms.netlify.app/K-POP-MIX/MIX-A/Q-15.html",
-                    "desc": "15번 한국노래 입니다.",
+                "Q-5": {
+                    "url": "https://ytms.netlify.app/K-POP-MIX/MIX-B/Q-5.html",
+                    "desc": "5번 한국노래 입니다.",
                     "quiz": "음악퀴즈",
                     "complite": 0,
                     "start": false
                 },
-                "Q-16": {
-                    "url": "https://ytms.netlify.app/K-POP-MIX/MIX-A/Q-16.html",
-                    "desc": "16번 한국노래 입니다.",
+                "Q-6": {
+                    "url": "https://ytms.netlify.app/K-POP-MIX/MIX-B/Q-6.html",
+                    "desc": "6번 한국노래 입니다.",
                     "quiz": "음악퀴즈",
                     "complite": 0,
                     "start": false
                 },
-                "Q-17": {
-                    "url": "https://ytms.netlify.app/K-POP-MIX/MIX-A/Q-17.html",
-                    "desc": "17번 한국노래 입니다.",
+                "Q-7": {
+                    "url": "https://ytms.netlify.app/K-POP-MIX/MIX-B/Q-7.html",
+                    "desc": "7번 한국노래 입니다.",
                     "quiz": "음악퀴즈",
                     "complite": 0,
                     "start": false
                 },
-                "Q-18": {
-                    "url": "https://ytms.netlify.app/K-POP-MIX/MIX-A/Q-18.html",
-                    "desc": "18번 한국노래 입니다.",
+                "Q-8": {
+                    "url": "https://ytms.netlify.app/K-POP-MIX/MIX-B/Q-8.html",
+                    "desc": "8번 한국노래 입니다.",
                     "quiz": "음악퀴즈",
                     "complite": 0,
                     "start": false
                 },
-                "Q-19": {
-                    "url": "https://ytms.netlify.app/K-POP-MIX/MIX-A/Q-19.html",
-                    "desc": "19번 한국노래 입니다.",
+                "Q-9": {
+                    "url": "https://ytms.netlify.app/K-POP-MIX/MIX-B/Q-9.html",
+                    "desc": "9번 한국노래 입니다.",
                     "quiz": "음악퀴즈",
                     "complite": 0,
                     "start": false
                 },
-                "Q-20": {
-                    "url": "https://ytms.netlify.app/K-POP-MIX/MIX-A/Q-20.html",
-                    "desc": "20번 한국노래 입니다.",
+                "Q-10": {
+                    "url": "https://ytms.netlify.app/K-POP-MIX/MIX-B/Q-10.html",
+                    "desc": "10번 한국노래 입니다.",
                     "quiz": "음악퀴즈",
                     "complite": 0,
                     "start": false
