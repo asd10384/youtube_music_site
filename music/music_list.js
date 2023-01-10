d15230e077d11a930fca58d310c91094fa814ca1
tkrmagid <tmdgks0466@naver.com>
2023-01-11 07:12:43 +0900

    리모델링

---
+++ music/music_list.js
@@ -0,0 +1,16 @@
+[
+  {
+    "K-POP": {
+      "desc": "K-POP 음악퀴즈",
+      "complite": 100,
+      "start": true
+    }
+  },
+  {
+    "J-POP": {
+      "desc": "J-POP 우타이테 음악퀴즈",
+      "complite": 100,
+      "start": true
+    }
+  }
+]
