c28e4c224dba1146e15526973c1015990c91c122
tkrmagid <tmdgks0466@naver.com>
2023-01-11 20:17:05 +0900

    custom img

--- music/music_list.js
+++ music/music_list.js
@@ -3,12 +3,20 @@
     "K-POP": {
       "desc": "K-POP 음악퀴즈",
       "complite": 100,
+      "customImage": false,
       "start": true
     },
     "J-POP": {
       "desc": "J-POP 우타이테 음악퀴즈",
       "complite": 100,
+      "customImage": false,
       "start": true
+    },
+    "게임음악": {
+      "desc": "게임 음악퀴즈",
+      "complite": 1,
+      "customImage": true,
+      "start": false
     }
   }
 ]
